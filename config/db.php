8c5922a6f4e4a830a1b82604c084f1cba8d69651
Rin-j-sun <146540554+Rin-j-sun@users.noreply.github.com>
2024-04-10 10:58:41 +0700

    Работа с базой данных
    Модернизация маршрутизации. Добавление файлов для подключения и работы с базой данных и вывода результата.

---
+++ config/db.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'driver' => 'mysql',
+    'host' => 'localhost',
+    'database' => 'rpyxnfsi_m1',
+    'username' => 'rpyxnfsi',
+    'password' => 'Uzn3ff',
+    'charset' => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix' => '',
+];
