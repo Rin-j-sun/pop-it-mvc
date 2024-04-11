84df4a7a88bd5c71ed0ef98e95a3aa5a348b0b21
Rin-j-sun <146540554+Rin-j-sun@users.noreply.github.com>
2024-04-11 17:21:33 +0700

    Вёрстка стартовой страницы

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -5,35 +5,80 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="/pop-it-mvc/public/style/style.css" type="text/css">
+    <link rel="stylesheet" href="/pop-it-mvc/public/style/style.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400&display=swap" rel="stylesheet">
     <title>Pop it MVC</title>
 </head>
 <body>
 <?php
-if (app()->auth::check()):
-if (app()->auth::checkRole()) echo 'ti loh';
-endif;
-?>
+if (!app()->auth::check()):
+    ?>
+    <header>
+
+    </header>
+    <main>
+<!--Возвращение данных стартовой страницы hello/php-->
+        <?= $content ?? '' ?>
+
+    </main>
 <header>
-    <nav>
-        <a href="<?= app()->route->getUrl('/hello') ?>">Главная</a>
-        <?php
-        if (!app()->auth::check()):
+    <?php
+    else:
+        if (app()->auth::checkRole()):
             ?>
-            <a href="<?= app()->route->getUrl('/login') ?>">Вход</a>
-<!--            <a href="--><?php //= app()->route->getUrl('/signup') ?><!--">Регистрация</a>-->
+            <header>
+                <nav>
+                    <div class="nav">
+                        <h1>Деканат<h1>
+                        <button class="button_nav"><a href="<?= app()->route->getUrl('/logout') ?>" class="button_nav_link">Выход</a></button>
+                    </div>
+
+                </nav>
+            </header>
+            <main>
+                <?= $content ?? '' ?>
+            </main>
         <?php
         else:
             ?>
-            <a href="<?= app()->route->getUrl('/logout') ?>">Выход (<?= app()->auth::user()->name ?>)</a>
+            <header>
+                <nav class="nav_employees">
+                    <h1>Деканат<h1>
+                            <button class = "nav_button">
+                                <a href="<?= app()->route->getUrl('/employees') ?>">Сотрудники</a>
+                            </button>
+                            <button class = "nav_button">
+                                <a href="<?= app()->route->getUrl('/hello') ?>">Личный кабинет</a>
+                            </button>
+                            <button class = "nav_button">
+                                <a href="<?= app()->route->getUrl('/logout') ?>">Выход</a>
+                            </button>
+                </nav>
+            </header>
+            <main>
+                <?= $content ?? '' ?>
+            </main>
         <?php
         endif;
         ?>
-    </nav>
-</header>
-<main>
-    <?= $content ?? '' ?>
-</main>
+
+    <?php
+    endif;
+    ?>
+
+    <?php
+    if (app()->auth::check()):
+        ?>
+        <footer>
+            <div class = "footer">
+                <a href="">О нас</a>
+                <p>2024г. Россия</p>
+            </div>
+        </footer>
+    <?php
+    endif;
+    ?>
 
 </body>
 </html>
--- views/site/hello.php
+++ views/site/hello.php
@@ -1,2 +1,62 @@
+<head>
+    <link rel="stylesheet" href="/pop-it-mvc/public/style/hello.css">
+</head>
 
-<h2><?= $message ?? ''; ?></h2>
+
+<?php
+if (!app()->auth::check()):
+    ?>
+    <!--Возвращение данных стартовой страницы main/php-->
+<div class="start_content">
+    <img class="owl" src="/pop-it-mvc/public/img/owl.png" alt="photo">
+
+    <h2>Добро Пожаловать В Деканат</h2>
+
+    <button class="button_orange"><a href="<?= app()->route->getUrl('/login') ?>" class="button_nav_link">Войти</a></button>
+
+    <img class="students" src="/pop-it-mvc/public/img/image 3.png" alt="photo">
+</div>
+<?php
+else:
+    if (app()->auth::checkRole()):
+        ?>
+        <div class="content_main_admin">
+            <div class="add_employee">
+                <h1>Добавьте новые данные</h1>
+                <div class="add_employee_info">
+                    <p>Добавьте нового сотрудника</p>
+                    <button class="button_add"><a href="<?= app()->route->getUrl('/addEmployees') ?>" class="button_add_link">Добавить</a></button>
+                </div>
+            </div>
+            <img src="/pop-it-mvc/public/image/main_admin.jpg" class="image" alt="Изображение">
+        </div>
+    <?php
+    else:
+        ?>
+        <main>
+            <div class="content_main_admin">
+                <div class="add_new_data">
+                    <h1>Добавьте новые данные</h1>
+                    <div class="add_new_data_info">
+                        <p>Добавьте нового студента</p>
+                        <button class="button_add"><a href="<?= app()->route->getUrl('/addStudents') ?>" class="button_add_link">Добавить</a></button>
+                    </div>
+                    <div class="add_new_data_info">
+                        <p>Добавьте новую группу</p>
+                        <button class="button_add"><a href="<?= app()->route->getUrl('/addGroup') ?>" class="button_add_link">Добавить</a></button>
+                    </div>
+                    <div class="add_new_data_info">
+                        <p>Добавьте новую дисциплину</p>
+                        <button class="button_add"><a href="<?= app()->route->getUrl('/addDiscipline') ?>" class="button_add_link">Добавить</a></button>
+                    </div>
+                </div>
+                <img src="/pop-it-mvc/public/image/main_employees.jpg" class="image" alt="Изображение">
+            </div>
+        </main>
+    <?php
+    endif;
+    ?>
+
+<?php
+endif;
+?>
