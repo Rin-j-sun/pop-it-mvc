327009d3e0c52aa7cb6212d12f270e7cabf0035f
Rin-j-sun <146540554+Rin-j-sun@users.noreply.github.com>
2024-04-11 21:59:22 +0700

    Вёрстка страницы добавления группы

--- views/employees/add_group.php
+++ views/employees/add_group.php
@@ -1,7 +1,14 @@
-<div class="add_group_content">
+<head>
+    <link rel="stylesheet" href="/pop-it-mvc/public/style/hello.css">
+    <link rel="stylesheet" href="/pop-it-mvc/public/style/employees/add_groupe.css">
+</head>
+
+
+<div class="add_discipline_content">
+    <div class="discipline_block"></div>
     <div class="add_group_content_block">
-        <h2>Добавление новой группы</h2>
-            <form method="post" class="form_add_group">
+        <h2>Добавить группу</h2>
+            <form method="post">
                 <input class="field_add_group" type="number" name="name" placeholder="Название">
                 <button class="button_add_group">Добавить</button>
             </form>
