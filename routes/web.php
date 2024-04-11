985f7c45544f1260057721dae3ffdb70661f407f
Rin-j-sun <146540554+Rin-j-sun@users.noreply.github.com>
2024-04-11 22:00:33 +0700

    Добавление маршрутизации

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,14 @@
 
 use Src\Route;
 
-Route::add('GET', '/hello', [Controller\Site::class, 'hello'])
-    ->middleware('auth');
-//Route::add(['GET', 'POST'], '/signup', [Controller\Site::class, 'signup']);
+Route::add('GET', '/hello', [Controller\Site::class, 'hello']);
+Route::add(['GET', 'POST'], '/addEmployees', [Controller\Admin::class, 'addEmployees'])->middleware('auth', 'role');
 Route::add(['GET', 'POST'], '/login', [Controller\Site::class, 'login']);
 Route::add('GET', '/logout', [Controller\Site::class, 'logout']);
-
+Route::add(['GET', 'POST'],'/addStudents', [Controller\Employees::class, 'addStudents'])->middleware('auth', 'roleEmployees');
+Route::add(['GET', 'POST'],'/addGroup', [Controller\Employees::class, 'addGroup'])->middleware('auth', 'roleEmployees');
+Route::add(['GET', 'POST'],'/addDiscipline', [Controller\Employees::class, 'addDiscipline'])->middleware('auth', 'roleEmployees');
+Route::add('GET','/students', [Controller\Employees::class, 'students'])->middleware('auth', 'roleEmployees');
+Route::add('GET','/groups', [Controller\Employees::class, 'groups'])->middleware('auth', 'roleEmployees');
+Route::add('GET','/disciplines', [Controller\Employees::class, 'disciplines'])->middleware('auth', 'roleEmployees');
+Route::add('GET','/gradeStudents', [Controller\Employees::class, 'gradeStudents'])->middleware('auth', 'roleEmployees');
